1c90ab9cccd7ae4fcfc6cb82da74ce895d8c4a57
Youssef <youssefelsorgi@gmail.com>
2024-04-25 16:45:43 +0200

    upload file

--- searchs/search.php
+++ searchs/search.php
@@ -4,15 +4,13 @@
 /*
   Search : 
       - Magic Methods --
-      - Difference Between abstract and interface 
-      - Polymorphism
+      - Difference Between abstract and interface --
+      - Polymorphism 
       - What is instanceof keyword in PHP --
       - can we define a constructor as a private constructor? if it yes when could we do this? --
       - constructor promotion --
       - does php has overloading & how to reach out if exists?  -- 
       - does php has multiple constructor or not and how to reach out --
-      - (mandatory) differences between interface abstract class
   */
 
-// link Google Drive =>   
-
+// link Google Drive =>  
